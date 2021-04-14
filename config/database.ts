d6abe0bbc1780a5e8bae03d787d44aa2f2e126e1
Marcus Vinicius O. Freire <vinny.freire@gmail.com>
2021-04-14 09:58:47 -0300

    disable ssl database

--- config/database.ts
+++ config/database.ts
@@ -42,7 +42,7 @@ const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
-        ssl: true,
+        ssl: false,
       },
       healthCheck: false,
       debug: false,
