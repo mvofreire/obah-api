139e13600754639ae8641857bcc3f513d12e3731
Marcus Vinicius O. Freire <vinny.freire@gmail.com>
2021-04-15 10:16:17 -0300

    add -f on run migration

--- config/database.ts
+++ config/database.ts
@@ -42,7 +42,7 @@ const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
-        ssl: { rejectUnauthorized: false },
+        ssl: Env.get('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : false,
       },
       healthCheck: false,
       debug: true,
