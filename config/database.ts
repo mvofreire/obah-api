4b709a5c4e36c2ba1cff8da385d9dfffa99e6068
Marcus Vinicius O. Freire <vinny.freire@gmail.com>
2021-04-15 10:25:38 -0300

    change statement for check production env

--- config/database.ts
+++ config/database.ts
@@ -42,7 +42,7 @@ const databaseConfig: DatabaseConfig & { orm: Partial<OrmConfig> } = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
-        ssl: Env.get('NODE_ENV') !== 'production' ? false : { rejectUnauthorized: false },
+        ssl: Env.get('NODE_ENV') !== 'production' ? { rejectUnauthorized: false } : true,
       },
       healthCheck: false,
       debug: true,
